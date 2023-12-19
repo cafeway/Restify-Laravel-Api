8d8ebb029df6cba50f3b026e6dcd50d71e580689
cafeway <bcaranja@gmail.com>
2023-12-19 10:22:28 +0300

    i installed laravel sanctum which will be used to generate api tokens for authenticating api requests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,5 +124,10 @@
                 </div>
             </div>
         </div>
+        <div id="swagger-api">
+
+            </div>
+            <script src="{{mix('js/swagger.js')}}"></script>
+            @vite('resources/js/swagger.js')
     </body>
 </html>
